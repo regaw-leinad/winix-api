3359b9d8e51a8671396f1b39da4fdd08f18a9dda
Dan Wager <danielwager@gmail.com>
2024-01-17 00:11:23 -0800

    add types to index

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import { Airflow, AirQuality, DeviceStatus, Mode, Plasmawave, Power } from './device';
+import { WinixAccount, WinixExistingAuth } from './account/winix-account';
+import { WinixAuth, WinixAuthResponse } from './account/winix-auth';
+import { WinixDevice } from './account/winix-device';
 import { WinixAPI } from './api';
 
 export {
+  WinixDevice,
+  WinixAccount,
+  WinixExistingAuth,
+  WinixAuth,
+  WinixAuthResponse,
   WinixAPI,
   Power,
   Mode,
